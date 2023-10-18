6ec5739275e75cfd1732a7c9cdaba0bce9d236cb
Artur Z <arturziianbaev@gmail.com>
2023-10-18 09:58:06 -0700

    find all items to buy and get their id

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ driver.get('https://orteil.dashnet.org/experiments/cookie/')
 time.sleep(1)
 
 cookie=driver.find_element(By.XPATH,value='//*[@id="cookie"]')
-for _ in range(1000):
+items=driver.find_elements(By.CSS_SELECTOR,'#store div')
+items_id=[item.get_attribute('id') for item in items]
+print(items_id)
+for _ in range(10):
 
     cookie.click()
+
+driver.close()
